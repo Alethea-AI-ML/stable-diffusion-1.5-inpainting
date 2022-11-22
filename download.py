8aca9ab5c38418eaca1224009c0569aa62cdc8ba
ajayjatav777 <107104050+ajayjatav777@users.noreply.github.com>
2022-11-22 17:39:48 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
 
-from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
+from diffusers import StableDiffusionInpaintPipeline
 import os
 
 def download_model():
@@ -9,17 +9,13 @@ def download_model():
     #Set auth token which is required to download stable diffusion model weights
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
 
-    lms = LMSDiscreteScheduler(
-        beta_start=0.00085, 
-        beta_end=0.012, 
-        beta_schedule="scaled_linear"
-    )
+#     lms = LMSDiscreteScheduler(
+#         beta_start=0.00085, 
+#         beta_end=0.012, 
+#         beta_schedule="scaled_linear"
+#     )
 
-    model = StableDiffusionPipeline.from_pretrained(
-        "runwayml/stable-diffusion-inpainting", 
-        scheduler=lms,
-        use_auth_token=HF_AUTH_TOKEN
-    )
+        model = StableDiffusionInpaintPipeline.from_pretrained("runwayml/stable-diffusion-inpainting",revision="fp16",torch_dtype=torch.float16, use_auth_token=HF_AUTH_TOKEN)
 
 if __name__ == "__main__":
     download_model()
